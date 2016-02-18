b1578c6aa43b4880ec00b1c3e3938a627ad3a83b
Kannan Thambiah <pygospa@gmail.com>
2016-02-18 02:45:21 +0100

    Add documentation

--- lib/hola_pygospa.rb
+++ lib/hola_pygospa.rb
@@ -1,4 +1,14 @@
+# The main Hola driver
 class Hola
+  # Say hi to the world!
+  #
+  # Example:
+  #   >> Hola.hi("spanish")
+  #   => ¡Hola mundo!
+  #
+  # Arguments:
+  #   language: (String)
+
   def self.hi(language = "english")
     translator = Translator.new(language)
     translator.hi
